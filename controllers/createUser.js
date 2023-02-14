d9b959833e9120baa0ebaf2c6d5ab7336924d19a
Bremi49 <remibouilly@hotmail.fr>
2023-02-14 13:43:13 +0000

    Modif sur les mail existant mais sans message pour le user

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -11,7 +11,7 @@ export default async(req, res) => {
     // Hash the password with bcrypt
     const hashedPassword = await bcrypt.hash(mdp, saltRounds);
     
-// Determine le role
+    // Determine le role
     let role;
     if (req.body.role === 'admin') {
         role = ADMIN_ROLE;
@@ -19,29 +19,31 @@ export default async(req, res) => {
         role = SUPERADMIN_ROLE;
     } else {
         // Handle the case where the role is not recognized
-        return res.status(400).send({ error: 'Invalid role specified' });
-    }
-    // Check if the email already exists in the database
-    const checkEmailSql = "SELECT id FROM Users WHERE mail = ?";
-    try {
-        const checkEmailResult = await asyncQuery(checkEmailSql, [mail]);
-        if (checkEmailResult.length > 0) {
-            // The email already exists, return an error response
-            return res.status(400).send({ error: 'Email already exists' });
-        }
-    } catch (err) {
-        console.log(err);
-        return;
+        return res.status(400).send({ error: 'Il faut choisir un admin' });
     }
     
+    // Check if the email already exists in the database
+    const checkEmailSql = "SELECT mail FROM Users WHERE mail = ?";
+        try {
+          const checkEmailResult = await asyncQuery(checkEmailSql, [mail]);
+          if (checkEmailResult.length > 0) {
+            console.log('Email déjà existant'); // Ajoutez un log pour vérifier que ce bloc est atteint
+            // The email already exists, return an error response
+            return
+          }
+        } catch (err) {
+          console.log(err);
+          return res.status(500).send({ error: 'Internal server error' });
+        }
+    // Create the user in the database
     const sql = "INSERT INTO Users (roles_id,mail,mdp) VALUES (?,?,?)"
     try {
         const paramsSql = [role,mail, hashedPassword]
         const createContact = await asyncQuery(sql, paramsSql)
-        return { response: createContact }
+        return res.status(200).send({ response: 'Creation du User' });
     }
     catch (err) {
         console.log(err)
-        return;
+        return res.status(500).send({ error: 'Internal server error' });
     }
 }
--- front/src/components/createUser.jsx
+++ front/src/components/createUser.jsx
@@ -1,39 +1,45 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { BASE_URL } from "../tools/constante.js"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../tools/constante.js";
 
 const CreateUser = () => {
-    const initialState = {role:'' , mail:'',  mdp: ''}
-    const [createUser, setCreateUser] = useState(initialState)
-    
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setCreateUser({ ...createUser, [name]: value })
-    }
+  const initialState = { role: "", mail: "", mdp: "" };
+  const [createUser, setCreateUser] = useState(initialState);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const submit = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCreateUser({ ...createUser, [name]: value });
+  };
+  
+  const submit = (e) => {
+    e.preventDefault();
+    axios.post(`${BASE_URL}/createUser`, createUser).then((res) => {
+      if (res.data.response) {
+        localStorage.setItem("response", JSON.stringify(res.data.response));
+        setCreateUser(initialState);
+      } else if (res.data.error === "Le mail existe") {
+        setErrorMessage("");
+      }
+    }).catch(error => {
+      console.error(error);
+      setErrorMessage("Erreur lors de la création de l'utilisateur");
+    });
+  };
 
-        e.preventDefault()
-        axios.post(`${BASE_URL}/createUser`, createUser)
-            .then(res => {
-                if (res.data.response) {
-                    const localStorage = (data) => {
-                        localStorage.setItem('response', JSON.stringify(res.data.response));
-                    }
-                }
-            })
-    }
-    return (
-        <form onSubmit={submit}>
-            <input type="email" name="mail" onChange={handleChange}  value={createUser.mail} />
-            <input type="password" name="mdp" onChange={handleChange} value={createUser.mdp} />
-            <select name="role" onChange={handleChange} value={createUser.role}>
-                <option value="">Sélectionnez un rôle</option>
-                <option value="superadmin">Admin+</option>
-                <option value="admin">Admin</option>
-            </select>
-            <input type="submit" />
-        </form>
-    )
+  return (
+    <form onSubmit={submit}>
+      {errorMessage.message && <div style={{ color: "red" }}>{errorMessage.message}</div>}
+      <input type="email" name="mail" onChange={handleChange} value={createUser.mail} />
+      <input type="password" name="mdp" onChange={handleChange} value={createUser.mdp} />
+      <select name="role" onChange={handleChange} value={createUser.role}>
+        <option value="">Sélectionnez un rôle</option>
+        <option value="superadmin">Admin+</option>
+        <option value="admin">Admin</option>
+      </select>
+      <input type="submit" />
+    </form>
+  );
 }
-export default CreateUser
+
+export default CreateUser;
